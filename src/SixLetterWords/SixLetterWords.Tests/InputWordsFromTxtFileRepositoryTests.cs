5ef358cf0178f10caaf5b17fb97d088ba3c3700a
Tom Ghekiere <tom.ghekiere@recomatics.com>
2020-05-14 12:57:29 +0200

    reading all input words

---
+++ src/SixLetterWords/SixLetterWords.Tests/InputWordsFromTxtFileRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace SixLetterWords.Tests
+{
+    public class InputWordsFromTxtFileRepositoryTests
+    {
+      [Fact]
+      public void Can_create() {
+        Assert.NotNull(new InputWordsFromTxtFileRepository(null));
+      }
+
+      [Fact]
+      public void Can_read_all_words() {
+        var sut =  new InputWordsFromTxtFileRepository(new InputWordsFromTxtFileConfiguration());
+
+        var words = sut.GetAll();
+
+        Assert.Equal(4202, words.Length);
+      }
+    }
+}
--- src/SixLetterWords/SixLetterWords/InputWordsRepository.cs
+++ src/SixLetterWords/SixLetterWords/InputWordsRepository.cs
@@ -1,4 +1,22 @@
 ﻿namespace SixLetterWords {
-  class InputWordsRepository {
+
+  public class InputWordsFromTxtFileConfiguration {
+    public string FilePath  => "input.txt";
+  }
+
+  class InputWordsFromTxtFileRepository : IInputWordsRepository{
+    private readonly InputWordsFromTxtFileConfiguration _config;
+
+    public InputWordsFromTxtFileRepository(InputWordsFromTxtFileConfiguration config) {
+      _config = config;
+    }
+    public string[] GetAll() {
+      var words = System.IO.File.ReadAllLines(_config.FilePath);
+      return words;
+    }
+  }
+
+  public interface IInputWordsRepository {
+    string[] GetAll();
   }
 }
